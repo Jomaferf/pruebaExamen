23c1296154854ec03b608cc74d53a3f897c39a37
Jomaferf <inalfe.mov04@gmail.com>
2024-05-16 23:08:16 +0200

    Subiendo clase sin paquete

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,10 +1,9 @@
-package holaMundo;
-
 public class HolaMundo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo!!!!.");
-	}
-
+	} 
 }
+
+	
